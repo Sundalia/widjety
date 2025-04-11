06f6f7a6a4e562fd7ae62bcbf150785db149e3ee
Sundahlia <84792932+Sundalia@users.noreply.github.com>
2025-04-11 19:34:25 +0300

    правки из trello

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ const takeOut = document.getElementById('take_out')
 const dialog = document.getElementById('dialog')
 const input = document.getElementById('input')
 const success = document.getElementById('success')
+const submit = document.getElementById('submit')
+const dialogClose = document.getElementById('close_popup_btn')
+const successClose = document.getElementById('close_success_btn')
 
 
 const token = import.meta.env.VITE_TG_TOKEN
@@ -20,32 +23,43 @@ const mask = new IMask(input, {
     mask: '+7(000)000-00-00'
 })
 
+const delay = ()=>{
+    return new Promise(resolve => setTimeout(resolve,  3000))
+}
 
-spline.load('https://prod.spline.design/m52CqUnTm6OcO98L/scene.splinecode').then(()=>{
-    canvas.addEventListener("click", (e) => {
-        takeOut.style.display='flex'
-    })
-    canvas.addEventListener("touchend", (e) => {
-        takeOut.style.display='flex'
-    })
-    takeOut.addEventListener('click', (e) => {
-        dialog.style.display='flex'
-    })
-    document.getElementById('submit').onclick = () => {
-        dialog.style.display='none'
-        const data = input.value
+await delay().then(()=> {
+    spline.load('https://prod.spline.design/m52CqUnTm6OcO98L/scene.splinecode').then(() => {
+        canvas.addEventListener("click", (e) => {
+            takeOut.style.display = 'flex'
+        })
+        canvas.addEventListener("touchend", (e) => {
+            takeOut.style.display = 'flex'
+        })
+        takeOut.addEventListener('click', (e) => {
+            dialog.style.display = 'flex'
+        })
+        submit.onclick = () => {
+            dialog.style.display = 'none'
+            const data = input.value
 
-        axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-            chat_id: chatId,
-            text: `${data}`,
-        }).then((response) => {
-            console.log('sent')
-        }).catch((error) => {
-            console.log(error)
+            axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+                chat_id: chatId,
+                text: `${data}`,
+            }).then((response) => {
+                console.log('sent')
+            }).catch((error) => {
+                console.log(error)
+            })
+            success.style.display = 'flex'
+            success.addEventListener('click', () => {
+                success.style.display = 'none'
+            })
+        }
+        dialogClose.addEventListener('click', () => {
+            dialog.style.display = 'none'
         })
-        success.style.display='flex'
-        success.addEventListener('click', ()=>{
-            success.style.display='none'
+        successClose.addEventListener("click", () => {
+            success.style.display = 'none'
         })
-    }
+    })
 })
